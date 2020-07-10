2943b10a70ffbf2b59e819c7c1285aba6f28d661
yule93 <dbfm0104@gmail.com>
2020-07-10 20:18:50 +0900

    plus color class
    css, 기능 추가!

--- src/components/colorPalette.js
+++ src/components/colorPalette.js
@@ -3,12 +3,12 @@ import './colorPalette.css';
 
 const Color = ({color, active, onClick}) => {
   return (
-      <div>
+      <div className = {`color $ {active && 'active'}`} style = {{background: color}} onClick = {onClick}>
 
       </div>
   );
 };
 
 const colorPalette = ({colors, selected, onSelect}) => {
-  
+
 }
